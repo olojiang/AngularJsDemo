b44112a1ecae24c2d5d53b4bf88c03aca7de867c
jiwei <bjjiwei@cn.ibm.com>
2015-11-15 20:11:58 +0800

    Add more directive example: tabs control

---
+++ app/js/tabs.js
@@ -0,0 +1,79 @@
+"use strict";
+
+var myModule = angular.module('myModule', []);
+myModule.controller("myController", ['$scope', function ($scope) {
+
+}]);
+
+myModule.directive("myTabs", [function () {
+    var directive = {
+    };
+
+    directive.replace = true; // default is true
+
+    directive.restrict = 'E'; // E-Element, A-Attribute, C-Class
+    directive.templateUrl = 'myTabsTemplate.html';
+
+    directive.transclude = true; //Show inner html into transclude part, use outer scope
+
+    // directive own controller
+    directive.controller = ['$scope', function ($scope) {
+        var panes = $scope.panes = [];
+
+        $scope.select = function (pane) {
+            angular.forEach(panes, function (value, key) {
+                value.selected = false;
+            });
+            pane.selected = true;
+        };
+
+        /**
+         * It's function for child directive to add themselves
+         * @param pane
+         */
+        this.addPane = function (pane) {
+            if(panes.length === 0) {
+                $scope.select(pane);
+            }
+            $scope.panes.push(pane);
+        };
+    }];
+
+    return directive;
+}]);
+
+myModule.directive("tab", [function () {
+    var directive = {};
+
+    directive.replace = true; // default is true
+
+    directive.restrict = 'E'; // E-Element, A-Attribute, C-Class
+    directive.templateUrl = 'tabTemplate.html';
+
+    directive.transclude = true; //Show inner html into transclude part, use outer scope
+
+    directive.require = ['^myTabs']; // require = '^parentController'
+
+    directive.scope = {
+        title: '@' // isolatedScopeVariable: '=attributeName', , '&attributeName' let attribute value evaluated with outer scope
+    };
+
+    directive.link = function (scope, element, attrs, controllers) {
+        // link(), scope to element for data binding, it has scope and its values, manipulate DOM
+        var controller = controllers[0];
+
+
+        // When element destroyed, clear memory, etc
+        element.on('$destroy', function () {
+
+        });
+    };
+
+    directive.link = function (scope, element, attrs, controllers) {
+        // link(), scope to element for data binding, it has scope and its values, manipulate DOM
+        var myTabs = controllers[0];
+        myTabs.addPane(scope);
+    };
+
+    return directive;
+}]);
