0b1fe9dfea96bf4ddec9cf74179281a61f2eb80f
jiwei <bjjiwei@cn.ibm.com>
2015-11-15 18:57:43 +0800

    Add more directive example.

--- app/js/directive.js
+++ app/js/directive.js
@@ -24,6 +24,40 @@ myModule.directive("userinfo", function () {
     return directive;
 });
 
+myModule.directive("userinfo2", function () {
+    var directive = {};
+
+    directive.replace = true; // default is true
+
+    directive.restrict = 'E'; // E-Element, A-Attribute, C-Class
+    directive.templateUrl = function (elem, attr) {
+        return 'template_' + attr.name + ".html";
+    };
+
+    return directive;
+});
+
+myModule.directive("dialogClose", [function () {
+    var directive = {};
+
+    directive.replace = true; // default is true
+    directive.transclude = true;
+
+    directive.restrict = 'E'; // E-Element, A-Attribute, C-Class
+
+    // following {message: 'hide for now'}, PASS child parameter -> parent
+    directive.template = "<div><span ng-click=\"close({message: 'hide for now'})\">&times;</span> Click left to hide<div ng-transclude></div></div> ";
+
+    // isolated scope
+    directive.scope = {
+        close: '&onClose' // isolatedScopeVariable: '=attributeName', '&attributeName' let attribute value evaluated with outer scope
+    };
+
+
+    return directive;
+}]);
+
+
 myModule.directive("userInfoIsolated", function () {
     var directive = {};
 
@@ -117,6 +151,41 @@ myModule.directive("elementLink", function () {
     return directive;
 });
 
+myModule.directive("elementLink2", ['$interval', 'dateFilter', function ($interval, dateFilter) {
+    var directive = {};
+
+    // need following when override the control $validate
+    directive.link = function (scope, element, attrs) {
+        // link(), scope to element for data binding, it has scope and its values, manipulate DOM
+
+        var format, intervalId;
+        scope.$watch(attrs.format, function (newValue, oldValue) {
+            format = newValue;
+
+            scope.updateTime();
+        });
+
+        scope.updateTime = function () {
+            element.html('Time: ' + dateFilter(new Date(), format));
+        };
+
+        // When element destroyed, clear memory, etc
+        element.on('$destroy', function () {
+            if(intervalId) {
+                $interval.cancel(intervalId);
+            }
+        });
+
+        // Initialize the timer
+        intervalId = $interval(function(){
+            scope.updateTime();
+        }, 1000);
+    };
+
+    return directive;
+}]);
+
+
 /*
  * Transclude
  * - When you want to warp the children of the element to the child
@@ -127,7 +196,7 @@ myModule.directive("elementTransclude", function () {
     directive.replace = true; // default is true
 
     directive.restrict = 'E'; // Element
-    directive.template = "<div>Outer: Hello, {{item.label}}, inner: <div ng-transclude></div></div>";
+    directive.template = "<div>Isolate Scope: Value: {{item.label}}<div ng-transclude></div></div>";
 //    directive.templateUrl = 'directiveTemplate.html';
 
     // isolated scope
@@ -141,7 +210,7 @@ myModule.directive("elementTransclude", function () {
 });
 
 
-myModule.controller("myController", function ($scope) {
+myModule.controller("myController", ['$scope', '$timeout', function ($scope, $timeout) {
     $scope.textToInsert = "Some text from controller.";
 
     // following is for isolated scope of directive
@@ -159,4 +228,22 @@ myModule.controller("myController", function ($scope) {
     // following is for transclude
     $scope.eee = {label: "EEE"};
     $scope.fff = {label: "FFF"};
-});
+
+    $scope.user = {
+        format: 'M/d/yy h:mm:ss a'
+    };
+
+    $scope.isHiding = false;
+    $scope.message = 'Display now';
+    $scope.name = "Ji Wei";
+
+    $scope.hideSection = function (message) {
+        $scope.isHiding = true;
+        $scope.message = message;
+
+        $timeout(function(){
+            $scope.isHiding = false;
+            $scope.message = 'Display now';
+        }, 2000);
+    };    
+}]);
---
+++ app/js/dragable.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var myModule = angular.module('myModule', []);
+myModule.directive("myDragable", ['$document', function ($document) {
+    var directive = {};
+
+    directive.replace = true; // default is true
+
+    directive.restrict = 'A'; // E-Element, A-Attribute, C-Class
+    directive.template = "<div ng-transclude></div>";
+
+    directive.transclude = true; //Show inner html into transclude part, use outer scope
+
+    directive.link = function (scope, element, attrs) {
+        // link(), scope to element for data binding, it has scope and its values, manipulate DOM
+        var startX = 0;
+        var startY = 0;
+        var x = 0, y = 0;
+
+        // Element basic style setting
+        element.css({
+            position: 'relative',
+            display: 'inline-block',
+            padding: '10px',
+            border: '1px solid gray',
+            backgroundColor: 'lightgrey',
+            cursor: 'pointer'
+        });
+
+        // Element event interaction
+        element.on('mousedown', function(event){
+            // Prevent default dragging of selected content
+            event.preventDefault();
+
+            startX = event.pageX - x;
+            startY = event.pageY - y;
+
+            $document.on('mousemove', mousemove);
+            $document.on('mouseup', mouseup);
+        });
+
+        function mousemove(event) {
+            x = event.pageX - startX;
+            y = event.pageY - startY;
+
+            element.css({
+                top: y+'px',
+                left: x + 'px'
+            });
+        }
+
+        function mouseup(event) {
+            $document.off('mouseup');
+            $document.off('mousemove');
+        }
+
+        // When element destroyed, clear memory, etc
+        element.on('$destroy', function () {
+
+        });
+    };
+
+    return directive;
+}]);
+
+myModule.controller("myController", ['$scope', function ($scope) {
+
+}]);
